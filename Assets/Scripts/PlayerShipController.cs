6b429d9a81e02ef8ebf82bd7a81082635fe58886
Harrison C Parker <hcp111@me.com>
2019-03-07 18:13:32 +0000

    Add player and movement

---
+++ Assets/Scripts/PlayerShipController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipController : MonoBehaviour
+{
+    public float rotationSpeed = 50;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
